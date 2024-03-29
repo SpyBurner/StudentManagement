afe391fe711586624061850e611ad39e4c7f4d7c
SpyBurner <zombiepigman1505@gmail.com>
2024-03-29 20:20:04 +0700

    before student list * change

--- University.h
+++ University.h
@@ -31,27 +31,36 @@ public:
         insertType(n3, EXAM);
     }
 
-    ~Semester(){
+    Semester(vector<int> TEMPLATE) {
+        insertType(TEMPLATE[0], ASSIGNMENT);
+        insertType(TEMPLATE[1], TEST);
+        insertType(TEMPLATE[2], EXAM);
+    }
+
+    ~Semester() {
         scoreMap.clear();
     }
 
-    void constructByInput(){
+    void constructByInput() {
         float temp;
 
-        cout << "Input " << scoreMap[ASSIGNMENT].size() << " ASSIGNMENT score(s), separated by spaces: ";
-        for (int i = 0; i < scoreMap[ASSIGNMENT].size(); ++i){
+        cout << endl
+             << "Input " << scoreMap[ASSIGNMENT].size() << " ASSIGNMENT score(s), separated by spaces: ";
+        for (int i = 0; i < scoreMap[ASSIGNMENT].size(); ++i) {
             cin >> temp;
             scoreMap[ASSIGNMENT][i] = temp;
         }
 
-        cout << "Input " << scoreMap[TEST].size() << " TEST score(s), separated by spaces: ";
-        for (int i = 0; i < scoreMap[TEST].size(); ++i){
+        cout << endl
+             << "Input " << scoreMap[TEST].size() << " TEST score(s), separated by spaces: ";
+        for (int i = 0; i < scoreMap[TEST].size(); ++i) {
             cin >> temp;
             scoreMap[TEST][i] = temp;
         }
 
-        cout << "Input " << scoreMap[EXAM].size() << " EXAM score(s), separated by spaces: ";
-        for (int i = 0; i < scoreMap[EXAM].size(); ++i){
+        cout << endl
+             << "Input " << scoreMap[EXAM].size() << " EXAM score(s), separated by spaces: ";
+        for (int i = 0; i < scoreMap[EXAM].size(); ++i) {
             cin >> temp;
             scoreMap[EXAM][i] = temp;
         }
@@ -64,6 +73,7 @@ protected:
     string name;
 
     vector<Semester> semesterList;
+
 public:
     Student() {}
     Student(int id, string name) : id(id), name(name) {}
@@ -72,24 +82,29 @@ public:
     int getID() { return id; }
     string getName() { return name; }
 
-    void addSemester(Semester semester){
-        semesterList.push_back(semester);
-    }
-
-    Semester getSemesterByID(int semID){
+    Semester getSemesterByID(int semID) {
         semID--;
-        if (semID >= semesterList.size() || semID < 1){
+        if (semID >= semesterList.size() || semID < 1) {
             throw new exception("ID out of range");
         }
         return semesterList.at(semID);
     }
 
-    virtual void addSemesterByInput();
-    void removeSemesterByID(int semID){
+    void addSemester(Semester semester) {
+        semesterList.push_back(semester);
+    }
+
+    void addDataToTemplate() {
+        for (auto iter = semesterList.begin(); iter < semesterList.end(); ++iter) {
+            cout << "Semester: " << semesterList.end() - iter << endl;
+            iter->constructByInput();
+        }
+    }
+
+    void removeSemesterByID(int semID) {
         semID--;
         semesterList.erase(semesterList.begin() + semID);
     }
-
 };
 
 class UniStudent : public Student {
@@ -100,24 +115,31 @@ public:
     UniStudent() : Student() {
     }
 
-    UniStudent(int id, string name) : Student(id, name) {
+    UniStudent(int id, string name, int semCount) : Student(id, name) {
+        for (int i = 0; i < semCount; ++i) {
+            semesterList.emplace_back(semesterTemplate);
+        }
+
     }
 
     ~UniStudent();
-
-
 };
 
 class CollStudent : public Student {
 private:
     vector<int> semesterTemplate = {1, 1, 1};
 
-    
 public:
     CollStudent() : Student() {
     }
 
     CollStudent(int id, string name) : Student(id, name) {
+    }
+
+    CollStudent(int id, string name, int semCount) : Student(id, name) {
+        for (int i = 0; i < semCount; ++i) {
+            semesterList.emplace_back(semesterTemplate);
+        }
     }
 
     ~CollStudent();
@@ -127,6 +149,8 @@ class StudentList {
 protected:
     string filePath;
     list<Student *> stList;
+
+    int timelineID = 0;
 
 public:
     template <class T>
@@ -141,9 +165,20 @@ public:
         }
     }
 
-    virtual void addStudent(Student *);
-    virtual void removeStudentById(int id);
-    virtual void listStudent();
-    virtual void listBestStudents();
-    virtual void saveToFile();
+    void addStudent(Student* student){
+        stList.push_back(student);
+    }
+
+    void removeStudentById(int id){
+        for (auto iter : stList){
+            if (iter->getID() == id){
+                delete iter;
+                stList.erase(iter)
+                return;
+            }
+        }
+    }
+    void listStudent();
+    void listBestStudents();
+    void saveToFile();
 };
